a0770bfc3e436af2c553becf28b99b428c5fbd3f
Airik Leon <airik.leon@gmail.com>
2018-01-11 13:42:47 -0700

    pushing rating file

--- JPAProject/src/entities/Contact.java
+++ JPAProject/src/entities/Contact.java
@@ -1,32 +1,17 @@
 package entities;
-//test
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import java.util.List;
+
+import javax.persistence.OneToMany;
 
 public class Contact {
-	
 	private int id;
-	
 	private String address1;
-	
 	private String address2;
-	
 	private String city;
-	
 	private String state;
-	
 	private String zip;
-	
 	private String phone;
-	
 	private String email;
-	
 	private String latitude;
-	
 	private String longitude;
-	
-
 }
--- JPAProject/src/entities/User.java
+++ JPAProject/src/entities/User.java
@@ -2,12 +2,30 @@ package entities;
 //test
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+@Entity
 public class User {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
+	@Column(name="first_name")
 	private String firstName;
+	@Column(name="last_name")
 	private String lastName;
+	@ManyToOne
+	@JoinColumn(name="contact_id")
 	private Contact contact;
+	@OneToOne
+	@JoinColumn(name="rating_id")
 	private double rating;
+	@JoinColumn(name="privilege")
 	private boolean privilege; 
 	private String userName;
 	List<Business> associatedBusinesses;

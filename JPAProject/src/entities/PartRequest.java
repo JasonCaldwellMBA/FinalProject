688e951184233d1dd5fcaf44ad6c041cffb1d947
David Stumpe <dstumpe264@gmail.com>
2018-01-11 14:40:52 -0700

    more mapping, parts request to part

--- JPAProject/src/entities/PartRequest.java
+++ JPAProject/src/entities/PartRequest.java
@@ -1,5 +1,26 @@
 package entities;
+
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
 //test
+@Entity
 public class PartRequest {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
 	
+	@OneToOne
+	@JoinColumn(name="request_id")
+	private Request request;
+	
+	@OneToMany(mappedBy="partRequest")
+	private Collection<Part> parts;
 }

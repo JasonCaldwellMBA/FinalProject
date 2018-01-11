1e0e8ebb5f3a3eb256946090e2718338721e64de
Airik Leon <airik.leon@gmail.com>
2018-01-11 14:28:14 -0700

    updated sql

--- JPAProject/src/entities/Rating.java
+++ JPAProject/src/entities/Rating.java
@@ -1,5 +1,25 @@
 package entities;
 
-public class Rating {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Rating {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id; 
+	private double rating;
+	
+	
+	public double getRating() {
+		return rating;
+	}
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+	public int getId() {
+		return id;
+	}
 }

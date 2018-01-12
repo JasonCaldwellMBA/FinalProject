0cc82004750028a2e4be52f13455110efb7c3821
David Stumpe <dstumpe264@gmail.com>
2018-01-12 16:02:32 -0700

    began business component

---
+++ MVCProject/WebContent/app/appModule/business/business.component.js
@@ -0,0 +1,10 @@
+angular.module('appModule')
+.component('businessComponent', {
+	templateUrl : "app/appModule/business/business.component.html",
+	controllerAs : 'vm',
+	controller : {
+		var vm = this;
+		
+		
+	}
+})	

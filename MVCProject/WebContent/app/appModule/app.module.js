0ae58d773b86cfe9a88ed12f22b199dcbad11ae5
Jason Caldwell <jdcald13@gmail.com>
2018-01-12 16:11:11 -0700

    Create nav bar, end body, and page not found

--- MVCProject/WebContent/app/appModule/app.module.js
+++ MVCProject/WebContent/app/appModule/app.module.js
@@ -1,1 +1,25 @@
-angular.module('appModule', ['ngRoute']); 
+angular.module('appModule', ['ngRoute'])
+.config(function($routeProvider){
+	  $routeProvider
+	    .when('/home', {
+	      template : '<home></home>'
+	    })
+	    .when('/about', {
+	      template : '<about></about>'
+	    })
+	    .when('/contact', {
+	    	template : '<contact></contact>'
+	    })
+	    .when('/register', {
+	    	template : '<register></register>'
+	    })
+	    .when('/login', {
+	    	template : '<login></login>'
+	    })
+	    .when('/logout', {
+	    	template : '<logout></logout>'
+	    })
+	    .otherwise({
+	      template : '<not-found></not-found>'
+	    })
+	});

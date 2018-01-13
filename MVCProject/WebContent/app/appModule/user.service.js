6e2126019027e7854011cbe5e93e21413e80b464
invictvs <david.chirdon@gmail.com>
2018-01-13 14:45:13 -0700

    vehicle

--- MVCProject/WebContent/app/appModule/user.service.js
+++ MVCProject/WebContent/app/appModule/user.service.js
@@ -2,10 +2,29 @@ $angular.module('appModule')
 .factory('userService', function($http){
 	var service = {};
 	
-	service.show = function(id){
+	service.index = function(){
 		return $http({
-			method : 'GET',
-			url : 'api/user' + id
+			method : "GET",
+			url : "api/user"
 		})
 	}
+	service.show = function(id){
+		return $http({
+			method : "GET",
+			url : "api/user/" + id
+		})
+	}
+	service.indexRequests = function(id){
+		return $http({
+			method : "GET",
+			url : "api/user/" + id + "/request/"
+		})
+	}
+	return service;
 })
+
+service.addVehicle = function(id){
+	return $http({
+		method : "PUT"
+	})
+}
--- MVCProject/WebContent/app/appModule/user/user.component.js
+++ MVCProject/WebContent/app/appModule/user/user.component.js
@@ -2,20 +2,47 @@ angular.module('appModule')
 .component('userComponent', {
 	templateUrl : 'app/appModule/user/user.component.html',
 	controllerAs : 'vm',
-	controller : function(userComponent){
+	controller : function(userComponent, $routeParams){
 		var vm = this;
 
 		vm.requests = [];
 		
 		vm.user = null;
 		
-		vm.getUser = function(id){
+//=========================Get User=====================//
+		
+		var getUser = function(id){
 			userService.show(id)
 			.then(function(response){
 				vm.user = response.data;
 			})
+		}		
+
 		}
+		getUser(1);
+		
+//==========================Get Requests=================//
+		
+		var getRequests = function(id){
+			userService.indexRequests(id)
+			.then(function(response){
+				vm.requests = response.data;
+			})
 		}
-}); 
+		getRequests(1);
 		
+//======================Make Copy of Requests==============//
 		
+		vm.detailedRequest = function(request){
+			vm.copy = angular.copy(request);	
+		}
+		
+//==========================Get Vehicle=====================//
+		
+		vm.getVehicles = function(){
+			$location.path("user/" + $routeParams.id + "/vehicle"); 
+			}
+		}
+
+	
+})	

5928d6a5526faa9276e68b79ffff4eaba181fd0a
Jason Caldwell <jdcald13@gmail.com>
2018-01-13 13:15:07 -0700

    Add comments to JPQL queries

--- MVCProject/src/data/QuoteDAOImpl.java
+++ MVCProject/src/data/QuoteDAOImpl.java
@@ -26,11 +26,18 @@ public class QuoteDAOImpl implements QuoteDAO {
 	@Override
     public Set<Quote> index(int bid, int rid) {
         String query = "SELECT q FROM Quote q WHERE q.business.id = :bid AND q.business.active = true AND q.request.id = :rid AND q.request.active = true AND q.active = true";
-        
+        //q.description, q.estimate, q.postDate, q.expireDate, q.completeDate
         List<Quote> quotes = em.createQuery(query, Quote.class)
                             .setParameter("bid", bid)
                             .setParameter("rid", rid)
                             .getResultList();
+//        for (Quote quote : quotes) {
+//			System.out.println(quote.getDescription());
+//			System.out.println(quote.getEstimate());
+//			System.out.println(quote.getPostDate());
+//			System.out.println(quote.getExpireDate());
+//			System.out.println(quote.getCompleteDate());
+//		}
         return new HashSet<Quote>(quotes);
     }
 

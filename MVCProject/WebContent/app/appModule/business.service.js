18ca8b346e6096432625ac672363f07bb1baf56e
David Stumpe <dstumpe264@gmail.com>
2018-01-13 11:32:00 -0700

    problems with bcrypt

--- MVCProject/WebContent/app/appModule/business.service.js
+++ MVCProject/WebContent/app/appModule/business.service.js
@@ -2,12 +2,25 @@ angular.module('appModule')
 .factory('businessService', function($http){
 	var service = {};
 	
+	service.index = function(){
+		return $http({
+			method : "GET",
+			url : "api/business"
+		})
+	}
 	
 	service.show = function(id){
 		return $http({
 			method : "GET",
-			url : "/api/business/" + id
+			url : "api/business/" + id
 		})
 	}
+//	service.indexQuotes = function(id){
+//		return $http({
+//			method : "GET"
+//			url : "api/business/" + id + "quote/"
+//		})
+//	}
+	
 	return service;
 })

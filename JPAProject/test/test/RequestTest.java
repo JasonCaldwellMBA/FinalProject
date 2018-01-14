bc65fcea9013d84fe437c9f6b45bbf007f0fc730
Jason Caldwell <jdcald13@gmail.com>
2018-01-13 20:44:20 -0700

    Test request

--- JPAProject/test/test/RequestTest.java
+++ JPAProject/test/test/RequestTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -15,29 +16,32 @@ import entities.Request;
 public class RequestTest {
     private EntityManagerFactory emf;
     private EntityManager em;
+    private Request request; 
     
     @Before
       public void setUp() throws Exception {
         this.emf = Persistence.createEntityManagerFactory("AutoDB");
         this.em = emf.createEntityManager();
+        request = em.find(Request.class, 1);
     }
     
     @After
       public void tearDown() throws Exception { 
         this.em.close();
         this.emf.close();
+        request = null;
     }
+    
+    @Test
+    public void test_request() {
+    		assertEquals(1, request.getId());
+    }
+    
     
     @Test
     public void test_vehicle_mapping() {
-       Request request = em.find(Request.class, 1); 
         assertEquals("Muffler", request.getParts().get(0).getName().toString());
-        
     }
     
-    
-    @Test
-    public void test_request_mapping() {
-    }
     
 }

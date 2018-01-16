ff1648f4b8fa5bd7360efc4d0f60f457e9f7e462
Jason Caldwell <jdcald13@gmail.com>
2018-01-16 11:48:53 -0700

    Stub certification front end

---
+++ MVCProject/WebContent/app/appModule/business/certification/certification.component.js
@@ -0,0 +1,8 @@
+angular.module('appModule')
+    .component('certification', {
+        templateUrl: 'app/appModule/certification/certification.component.html',
+        controllerAs: 'vm',
+        controller: function () {
+            
+        }
+    })

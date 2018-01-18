173c072494c8618aa6bdb128a14c8c61cfe7c2d2
David Stumpe <dstumpe264@gmail.com>
2018-01-18 11:14:24 -0700

    changed sidebar navigation so it doesnt crash when info is not loaded yet

--- MVCProject/WebContent/app/appModule/business/businessRequest/businessRequest.component.js
+++ MVCProject/WebContent/app/appModule/business/businessRequest/businessRequest.component.js
@@ -2,7 +2,7 @@ angular.module('appModule')
 	.component('businessRequest', {
 		templateUrl: 'app/appModule/business/businessRequest/businessRequest.component.html',
 		controllerAs: 'vm',
-		controller: function (requestService, $routeParams, businessService, quoteService, distanceMatrixService, authService) {
+		controller: function (requestService, $routeParams, $location, businessService, quoteService, distanceMatrixService, authService) {
 			var vm = this;
 			vm.business = null;
 			vm.requests = [];
@@ -18,7 +18,7 @@ angular.module('appModule')
 			//init Methods
 			businessService.show($routeParams.id).then(function (res) {
 				vm.business = res.data;
-				console.log(vm.business);
+//				console.log(vm.business);
 				requestService.indexAllRequests().then(function (res) {
 					//UNCOMMENT FOR PRACTICAL PRESENTATION
 					// var MAX_DISTANCE = 50;
@@ -63,6 +63,32 @@ angular.module('appModule')
 				quoteService.createQuote(quote, vm.selected.id).then(function (res) {
 					vm.quoteFlag = null;
 				});
+			}
+			
+			//functions for sidebar routing
+			vm.home = function(){
+				$location.path("business/" + vm.bizId);
+			}
+			vm.viewAllQuotes = function(){
+				$location.path("business/" + vm.bizId + "/quote");
+			}
+			vm.viewPendingQuotes = function(){
+				$location.path("business/" + vm.bizId + "/pendingQuotes");
+			}
+			vm.viewAcceptedQuotes = function(){
+				$location.path("business/" + vm.bizId + "/acceptedQuotes");
+			}
+			vm.viewCompletedQuotes = function(){
+				$location.path("business/" + vm.bizId + "/completedQuotes");
+			}
+			vm.viewRequests = function(){
+				$location.path("business/" + vm.bizId + "/request");
+			}
+			vm.viewCertifications = function(){
+				$location.path("business/" + vm.bizId + "/certification");
+			}
+			vm.viewSettings = function(){
+				$location.path("business/" + vm.bizId + "/settings");
 			}
 		}
 	})

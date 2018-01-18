8ad8af783ed33407309775183284b9fec95082fc
Airik Leon <airik.leon@gmail.com>
2018-01-18 16:26:33 -0700

    fix colon

--- MVCProject/WebContent/app/appModule/request/requestDetail/requestDetail.component.js
+++ MVCProject/WebContent/app/appModule/request/requestDetail/requestDetail.component.js
@@ -30,7 +30,7 @@ angular.module('appModule')
                         business: quote.business,
                         user: quote.request.user,
                         message: vm.request.user.firstName + ' ' + vm.request.user.lastName + ' selected your quote for #Quote id: ' + quote.id + ', #Request id: ' + vm.request.id,
-                        type = "business"
+                        type : "business"
                     };
 
                     console.log(notification);
